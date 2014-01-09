1e32bfe05246d14dc352791d6958e871f2c649c2
Ryan French <rfrench@desktop8.ceeo.chapman.edu>
2014-01-09 07:35:29 -0800

    completed: hello world

--- exercises/lecture1/lecture1_homework/hello_world/kernel.cu
+++ exercises/lecture1/lecture1_homework/hello_world/kernel.cu
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 __global__ void mykernel(){
-	printf("Hello world from device!\n");
+	printf("Hello world from device block %i !\n",blockIdx.x);
 } /* end kernel */
 
 int main(void) 
 {
         /* launch this kernel 10 times*/
-	mykernel<<< ... , ...>>>();
+	mykernel<<< 2 , 5 >>>();
  	cudaDeviceSynchronize();
 	printf("Hello World from Host\n");
 	return 0;
